2e357b36c5da5a60e2fee693440a1089a577d717
Jesse Lee <mienaikoe@gmail.com>
2023-04-28 13:41:13 -0400

    remove old code

--- archive/_headers.php
+++
@@ -1,4 +0,0 @@
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<link rel="stylesheet" type="text/css" href="assets/index.css"/>
-<link rel="shortcut icon" href="/favicon.ico" />
-<script type="text/javascript" src="assets/index.js"></script>
